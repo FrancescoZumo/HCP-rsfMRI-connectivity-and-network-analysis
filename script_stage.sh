0cb007e43893da9fa3e904e9764ca90762d0a9a3
ZrancescoZumo <francescozumo@gmail.com>
2019-12-04 16:20:29 +0100

    now section 2 works, in sect 1 last line not working, killed

--- script_stage.sh
+++ script_stage.sh
@@ -10,11 +10,19 @@ Optional arguments
 \tSet a different path to data\n
 
 -h, --help
-\tShows usage\n"
+\tShows usage\n
+
+-i --interactive
+\tEnable interactive mode: you can choose which sections will be launched\n
+"
 }
 
 #Default path to HCP data 
 PATH2DATA="../Data"
+#will be set true if -i argument is used
+interactive=false
+#default value, so every section is executed
+flag=true
 
 #optional arguments
 while [[ "$1" != "" ]]; do
@@ -27,6 +35,10 @@ while [[ "$1" != "" ]]; do
 			usage
 			exit
 			;;
+		-i | --interactive )
+			interactive=true
+			echo "<interactive mode enabled>"
+			;;
 		* )
 			echo "Unknown option, Abort"
 			exit
@@ -35,23 +47,82 @@ while [[ "$1" != "" ]]; do
 	shift
 done
 
-#variable for storing the previous and the current subject checked
-current=""
-prev=""
+#INTERACTIVE : path to Data
+if [[ "$interactive" == true ]]; then
+	read -p "current path to Data: $PATH2DATA, 
+would you like to change it? [Y/n] " answer
+	case $answer in
+	[Yy] | [Yy][Ee][Ss] )
+		read -p "new path: " PATH2DATA
+		;;
+	[Nn] | [Nn][Oo])
+		echo "path confirmed"
+		;;
+	* )
+		echo "Invalid answer, Abort"
+		exit
+		;;
+	esac
+fi
 
-#creating a folder for each subject
-for entry in "$PATH2DATA"/*; do
-	entry=${entry#"$PATH2DATA/"}
-	current=${entry:0:6}
-	# here I assume that the for cycle orders entries by name, 
-	# so I will always get folders from the same subject consecutively
-	if [[ $current != $prev ]]; then
-		# I store a new subject only if the previous is different
-		mkdir $PATH2DATA/$current
-	fi
-	mv $PATH2DATA/$entry $PATH2DATA/$current
-	prev=$current
-done
+#INTERACTIVE : setting up Data folder
+if [[ "$interactive" == true ]]; then
+	read -p "should I set up Data folder? [Y/n] " answer
+	case $answer in
+	[Yy] | [Yy][Ee][Ss] )
+		echo "confirmed"
+		flag=true
+		;;
+	[Nn] | [Nn][Oo])
+		echo "declined"
+		flag=false
+		;;
+	* )
+		echo "Invalid answer, Abort"
+		exit
+		;;
+	esac
+fi
+
+if [[ "$flag" == true ]]; then
+	#variable for storing the previous and the current subject checked
+	current=""
+	prev=""
+
+	#creating a folder for each subject
+	for entry in "$PATH2DATA"/*; do
+		entry=${entry#"$PATH2DATA/"}
+		current=${entry:0:6}
+		# here I assume that the for cycle orders entries by name, 
+		# so I will always get folders from the same subject consecutively
+		if [[ $current != $prev ]]; then
+			# I store a new subject only if the previous is different
+			mkdir $PATH2DATA/$current
+		fi
+		mv $PATH2DATA/$entry $PATH2DATA/$current
+		prev=$current
+	done
+fi
+
+
+#INTERACTIVE : execute code for only one subject
+if [[ "$interactive" == true ]]; then
+	read -p "should I execute code for only one subject? [Y/n] " answer
+	case $answer in
+	[Yy] | [Yy][Ee][Ss] )
+		echo "confirmed"
+		oneshot=true
+		;;
+	[Nn] | [Nn][Oo])
+		echo "declined"
+		oneshot=false
+		;;
+	* )
+		echo "Invalid answer, Abort"
+		exit
+		;;
+	esac
+fi
 
 #commenta molto tutto il codice, spiega parametri scelti x ogni comando e perché lo stai facendo
 
@@ -60,54 +131,165 @@ for subject in "$PATH2DATA"/*; do
 	id=${subject#"$PATH2DATA/"}
 	PATH2RES="$subject/results"
 	rfMRI=$id_
-	mkdir $subject/results
+	mkdir -p $subject/results
 
 	#saving the path for each file that will be used
 	SBRef_dc_T1w="$subject/${id}_3T_rfMRI_REST1_preproc/$id/T1w/Results/rfMRI_REST1_LR/SBRef_dc.nii.gz"
 	rfMRI_REST1_LR_SBRef="$subject/${id}_3T_rfMRI_REST1_preproc/$id/MNINonLinear/Results/rfMRI_REST1_LR/rfMRI_REST1_LR_SBRef.nii.gz"
 	SBRef_dc="$subject/${id}_3T_rfMRI_REST1_preproc/$id/MNINonLinear/Results/rfMRI_REST1_LR/SBRef_dc.nii.gz"
 	T1w_acpc_dc_restore="$subject/${id}_3T_Structural_preproc/$id/T1w/T1w_acpc_dc_restore.nii.gz"
+	rfMRI_REST1_LR="$subject/${id}_3T_rfMRI_REST1_preproc/102614/MNINonLinear/Results/rfMRI_REST1_LR/rfMRI_REST1_LR.nii.gz"
 
-	#brain extraction
+	#INTERACTIVE : First section
+	if [[ "$interactive" == true ]]; then
+		read -p "start first section? [Y/n] " answer
+		case $answer in
+		[Yy] | [Yy][Ee][Ss] )
+			echo "confirmed"
+			flag=true
+			;;
+		[Nn] | [Nn][Oo])
+			echo "declined"
+			flag=false
+			;;
+		* )
+			echo "Invalid answer, Abort"
+			exit
+			;;
+		esac
+	fi
 
-	#these images contain whole head, I need to extract the brain for next operations (epi_reg, applyXFM)
-	printf "\nbet $SBRef_dc $PATH2RES/SBRef_dc_brain.nii.gz -R -f 0.65\n"
-	# for each bet, after som trials, I selected the best -f value
-	bet $SBRef_dc $PATH2RES/SBRef_dc_brain.nii.gz -R -f 0.65
-	printf "\nbet $SBRef_dc_T1w $PATH2RES/SBRef_dc_T1w_brain.nii.gz -R -f 0.5\n"
-	bet $SBRef_dc_T1w $PATH2RES/SBRef_dc_T1w_brain.nii.gz -R -f 0.5
-	printf "\nbet $T1w_acpc_dc_restore $PATH2RES/T1w_acpc_dc_restore_brain.nii.gz -R -f 0.2\n"
-	bet $T1w_acpc_dc_restore $PATH2RES/T1w_acpc_dc_restore_brain.nii.gz -R -f 0.2
+	if [[ "$flag" == true ]]; then
 
-	#flirt
-	#phase one: I want to match the SBRef file from fmri (res=2mm) with the one from sructural space (res=0.7mm)
-	#parameters: 12 dof, images: already virtually aligned, cost function: normalized mutual information
-	#
-	printf "\nflirt -in $PATH2RES/SBRef_dc_brain.nii.gz -ref $PATH2RES/SBRef_dc_T1w_brain.nii.gz -out $PATH2RES/flirt -omat $PATH2RES/flirt.mat -bins 256 -cost normmi -searchrx 0 0 -searchry 0 0 -searchrz 0 0 -dof 12  -interp trilinear\n"
-	flirt -in $PATH2RES/SBRef_dc_brain.nii.gz -ref $PATH2RES/SBRef_dc_T1w_brain.nii.gz -out $PATH2RES/flirt -omat $PATH2RES/flirt.mat -bins 256 -cost normmi -searchrx 0 0 -searchry 0 0 -searchrz 0 0 -dof 12  -interp trilinear
+		#fslroi
+		#explanation
+		printf "\nfslroi $rfMRI_REST1_LR $PATH2RES/rfMRI_REST1_LR_1180.nii.gz 20 -1\n"
+		fslroi $rfMRI_REST1_LR $PATH2RES/rfMRI_REST1_LR_1180.nii.gz 20 -1
 
-	#epi_reg
-	#phase two: I want to 
-	printf "\nepi_reg --epi=$PATH2RES/SBRef_dc_T1w_brain.nii.gz --t1=$T1w_acpc_dc_restore --t1brain=$PATH2RES/T1w_acpc_dc_restore_brain.nii.gz --out=$PATH2RES/epi2struct\n"
-	epi_reg --epi=$PATH2RES/SBRef_dc_T1w_brain.nii.gz --t1=$T1w_acpc_dc_restore --t1brain=$PATH2RES/T1w_acpc_dc_restore_brain.nii.gz --out=$PATH2RES/epi2struct
+		#brain extraction
+		#these images contain whole head, I need to extract the brain for next operations (epi_reg, applyXFM)
+		printf "\nbet $SBRef_dc $PATH2RES/SBRef_dc_brain.nii.gz -R -f 0.65\n"
+		# for each bet, after som trials, I selected the best -f value
+		bet $SBRef_dc $PATH2RES/SBRef_dc_brain.nii.gz -R -f 0.65
+		printf "\nbet $SBRef_dc_T1w $PATH2RES/SBRef_dc_T1w_brain.nii.gz -R -f 0.5\n"
+		bet $SBRef_dc_T1w $PATH2RES/SBRef_dc_T1w_brain.nii.gz -R -f 0.5
+		printf "\nbet $T1w_acpc_dc_restore $PATH2RES/T1w_acpc_dc_restore_brain.nii.gz -R -f 0.2\n"
+		bet $T1w_acpc_dc_restore $PATH2RES/T1w_acpc_dc_restore_brain.nii.gz -R -f 0.2
 
-	#Concatxfm
-	printf "\nconvert_xfm -omat $PATH2RES/finalMatrix.mat -concat $PATH2RES/epi2struct.mat $PATH2RES/flirt.mat \n"
-	convert_xfm -omat $PATH2RES/finalMatrix.mat -concat $PATH2RES/epi2struct.mat $PATH2RES/flirt.mat 
+		#flirt
+		#phase one: I want to match the SBRef file from fmri (res=2mm) with the one from sructural space (res=0.7mm)
+		#parameters: 12 dof, images: already virtually aligned, cost function: normalized mutual information
+		#
+		printf "\nflirt -in $PATH2RES/SBRef_dc_brain.nii.gz -ref $PATH2RES/SBRef_dc_T1w_brain.nii.gz -out $PATH2RES/flirt -omat $PATH2RES/flirt.mat -bins 256 -cost normmi -searchrx 0 0 -searchry 0 0 -searchrz 0 0 -dof 12  -interp trilinear\n"
+		flirt -in $PATH2RES/SBRef_dc_brain.nii.gz -ref $PATH2RES/SBRef_dc_T1w_brain.nii.gz -out $PATH2RES/flirt -omat $PATH2RES/flirt.mat -bins 256 -cost normmi -searchrx 0 0 -searchry 0 0 -searchrz 0 0 -dof 12  -interp trilinear
 
-	#metti questa, sopra ho scambiato e dovrebbe essere giusto, controlla x sicurezza
-	#convert_xfm -omat finedef -concat /home/francescozumo/Desktop/working_folder/epi2struct.mat /home/francescozumo/Desktop/working_folder/flirt.mat
+		#epi_reg
+		#phase two: I want to 
+		printf "\nepi_reg --epi=$PATH2RES/SBRef_dc_T1w_brain.nii.gz --t1=$T1w_acpc_dc_restore --t1brain=$PATH2RES/T1w_acpc_dc_restore_brain.nii.gz --out=$PATH2RES/epi2struct\n"
+		epi_reg --epi=$PATH2RES/SBRef_dc_T1w_brain.nii.gz --t1=$T1w_acpc_dc_restore --t1brain=$PATH2RES/T1w_acpc_dc_restore_brain.nii.gz --out=$PATH2RES/epi2struct
 
-	#ApplyXFM
-	#contrplla senza percorso assoluto
-	printf "\nflirt -in $rfMRI_REST1_LR_SBRef -applyxfm -init $PATH2RES/finalMatrix.mat -out $PATH2RES/rfMRI_REST1_LR_SBRef_matApplied -paddingsize 0.0 -interp trilinear -ref $PATH2RES/T1w_acpc_dc_restore_brain.nii.gz\n"
-	flirt -in $rfMRI_REST1_LR_SBRef -applyxfm -init $PATH2RES/finalMatrix.mat -out $PATH2RES/rfMRI_REST1_LR_SBRef_matApplied -paddingsize 0.0 -interp trilinear -ref $PATH2RES/T1w_acpc_dc_restore_brain.nii.gz
+		#Concatxfm
+		printf "\nconvert_xfm -omat $PATH2RES/finalMatrix.mat -concat $PATH2RES/epi2struct.mat $PATH2RES/flirt.mat \n"
+		convert_xfm -omat $PATH2RES/finalMatrix.mat -concat $PATH2RES/epi2struct.mat $PATH2RES/flirt.mat 
 
-	#da aggiungere
-	#fast T1w_acpc_dc_restore_brain.nii.gz
+		#metti questa, sopra ho scambiato e dovrebbe essere giusto, controlla x sicurezza
+		#convert_xfm -omat finedef -concat /home/francescozumo/Desktop/working_folder/epi2struct.mat /home/francescozumo/Desktop/working_folder/flirt.mat
 
-	#convert_xfm -omat $PATH2RES/inverseMatrix.mat -inverse $PATH2RES/finalMatrix.mat
+		#ApplyXFM
+		printf "\nflirt -in $rfMRI_REST1_LR_SBRef -applyxfm -init $PATH2RES/finalMatrix.mat -out $PATH2RES/rfMRI_REST1_LR_SBRef_matApplied -paddingsize 0.0 -interp trilinear -ref $PATH2RES/T1w_acpc_dc_restore_brain.nii.gz\n"
+		flirt -in $rfMRI_REST1_LR_SBRef -applyxfm -init $PATH2RES/finalMatrix.mat -out $PATH2RES/rfMRI_REST1_LR_SBRef_matApplied -paddingsize 0.0 -interp trilinear -ref $PATH2RES/T1w_acpc_dc_restore_brain.nii.gz
 
-	
+
+		#viene ucciso!
+
+		#Apply to rfMRI_REST1_LR_1180.nii.gz
+		#printf "\nflirt -in $PATH2RES/rfMRI_REST1_LR_1180.nii.gz -applyxfm -init $PATH2RES/finalMatrix.mat -out $PATH2RES/rfMRI_REST1_LR_1180_matApplied.nii.gz -paddingsize 0.0 -interp trilinear -ref $PATH2RES/T1w_acpc_dc_restore_brain.nii.gz\n"
+		#flirt -in $PATH2RES/rfMRI_REST1_LR_1180.nii.gz -applyxfm -init $PATH2RES/finalMatrix.mat -out $PATH2RES/rfMRI_REST1_LR_1180_matApplied.nii.gz -paddingsize 0.0 -interp trilinear -ref $PATH2RES/T1w_acpc_dc_restore_brain.nii.gz
+		
+
+
+	fi
+
+	#INTERACTIVE : Second section
+	if [[ "$interactive" == true ]]; then
+		read -p "start second section? [Y/n] " answer
+		case $answer in
+		[Yy] | [Yy][Ee][Ss] )
+			echo "confirmed"
+			flag=true
+			;;
+		[Nn] | [Nn][Oo])
+			echo "declined"
+			flag=false
+			;;
+		* )
+			echo "Invalid answer, Abort"
+			exit
+			;;
+		esac
+	fi
+
+	if [[ "$flag" == true ]]; then
+		#fast 
+		#description
+		printf "\nfast $PATH2RES/T1w_acpc_dc_restore_brain.nii.gz\n"
+		fast $PATH2RES/T1w_acpc_dc_restore_brain.nii.gz
+
+		#convert_xfm
+		#description
+		printf "\nconvert_xfm -omat $PATH2RES/inverseMatrix.mat -inverse $PATH2RES/finalMatrix.mat\n"
+		convert_xfm -omat $PATH2RES/inverseMatrix.mat -inverse $PATH2RES/finalMatrix.mat
+
+		#mat applied to T1w_brain
+		#description
+		printf "\nflirt -in $PATH2RES/T1w_acpc_dc_restore_brain.nii.gz -applyxfm -init $PATH2RES/inverseMatrix.mat -out $PATH2RES/T1w_acpc_dc_restore_brain_matApplied.nii.gz -paddingsize 0.0 -interp trilinear -ref $PATH2RES/SBRef_dc_brain.nii.gz\n"
+		flirt -in $PATH2RES/T1w_acpc_dc_restore_brain.nii.gz -applyxfm -init $PATH2RES/inverseMatrix.mat -out $PATH2RES/T1w_acpc_dc_restore_brain_matApplied.nii.gz -paddingsize 0.0 -interp trilinear -ref $PATH2RES/SBRef_dc_brain.nii.gz
+
+		#apply inverse mat to PVE_0
+		#description
+		printf "\nflirt -in $PATH2RES/T1w_acpc_dc_restore_brain_pve_0.nii.gz -applyxfm -init $PATH2RES/inverseMatrix.mat -out $PATH2RES/T1w_acpc_dc_restore_brain_pve_0_matApplied.nii.gz -paddingsize 0.0 -interp trilinear -ref $PATH2RES/SBRef_dc_brain.nii.gz\n"
+		flirt -in $PATH2RES/T1w_acpc_dc_restore_brain_pve_0.nii.gz -applyxfm -init $PATH2RES/inverseMatrix.mat -out $PATH2RES/T1w_acpc_dc_restore_brain_pve_0_matApplied.nii.gz -paddingsize 0.0 -interp trilinear -ref $PATH2RES/SBRef_dc_brain.nii.gz
+		
+		#apply inverse mat to pve_1
+		#description
+		printf "\nflirt -in $PATH2RES/T1w_acpc_dc_restore_brain_pve_1.nii.gz -applyxfm -init $PATH2RES/inverseMatrix.mat -out $PATH2RES/T1w_acpc_dc_restore_brain_pve_1_matApplied.nii.gz -paddingsize 0.0 -interp trilinear -ref $PATH2RES/SBRef_dc_brain.nii.gz\n"
+		flirt -in $PATH2RES/T1w_acpc_dc_restore_brain_pve_1.nii.gz -applyxfm -init $PATH2RES/inverseMatrix.mat -out $PATH2RES/T1w_acpc_dc_restore_brain_pve_1_matApplied.nii.gz -paddingsize 0.0 -interp trilinear -ref $PATH2RES/SBRef_dc_brain.nii.gz
+
+		#apply inverse mat to pve_2
+		#description
+		printf "\nflirt -in $PATH2RES/T1w_acpc_dc_restore_brain_pve_2.nii.gz -applyxfm -init $PATH2RES/inverseMatrix.mat -out $PATH2RES/T1w_acpc_dc_restore_brain_pve_2_matApplied.nii.gz -paddingsize 0.0 -interp trilinear -ref $PATH2RES/SBRef_dc_brain.nii.gz\n"
+		flirt -in $PATH2RES/T1w_acpc_dc_restore_brain_pve_2.nii.gz -applyxfm -init $PATH2RES/inverseMatrix.mat -out $PATH2RES/T1w_acpc_dc_restore_brain_pve_2_matApplied.nii.gz -paddingsize 0.0 -interp trilinear -ref $PATH2RES/SBRef_dc_brain.nii.gz
+
+		#fslmath pve_0
+		#description
+		printf "\nfslmaths $PATH2RES/T1w_acpc_dc_restore_brain_pve_0_matApplied.nii.gz -thr 0.9 -bin $PATH2RES/fslmaths_pve_0\n"
+		fslmaths $PATH2RES/T1w_acpc_dc_restore_brain_pve_0_matApplied.nii.gz -thr 0.9 -bin $PATH2RES/fslmaths_pve_0
+
+		#fslmaths pve_1
+		printf "\nfslmaths $PATH2RES/T1w_acpc_dc_restore_brain_pve_1_matApplied.nii.gz -thr 0.9 -bin $PATH2RES/fslmaths_pve_1\n"
+		fslmaths $PATH2RES/T1w_acpc_dc_restore_brain_pve_1_matApplied.nii.gz -thr 0.9 -bin $PATH2RES/fslmaths_pve_1
+
+		#fslmaths pve_2
+		printf "\nfslmaths $PATH2RES/T1w_acpc_dc_restore_brain_pve_2_matApplied.nii.gz -thr 0.9 -bin $PATH2RES/fslmaths_pve_2\n"
+		fslmaths $PATH2RES/T1w_acpc_dc_restore_brain_pve_2_matApplied.nii.gz -thr 0.9 -bin $PATH2RES/fslmaths_pve_2
+
+		#fslmeants pve_0
+		#description
+		printf "\nfslmeants -i $PATH2RES/rfMRI_REST1_LR_1180.nii.gz -o $PATH2RES/meants_pve_0.txt -m $PATH2RES/fslmaths_pve_0.nii.gz\n"
+		fslmeants -i $PATH2RES/rfMRI_REST1_LR_1180.nii.gz -o $PATH2RES/meants_pve_0.txt -m $PATH2RES/fslmaths_pve_0.nii.gz
+
+		#fslmeants pve_1
+		printf "\nfslmeants -i $PATH2RES/rfMRI_REST1_LR_1180.nii.gz -o $PATH2RES/meants_pve_0.txt -m $PATH2RES/fslmaths_pve_1.nii.gz\n"
+		fslmeants -i $PATH2RES/rfMRI_REST1_LR_1180.nii.gz -o $PATH2RES/meants_pve_1.txt -m $PATH2RES/fslmaths_pve_1.nii.gz
+
+		#fslmeants pve_2
+		printf "\nfslmeants -i $PATH2RES/rfMRI_REST1_LR_1180.nii.gz -o $PATH2RES/meants_pve_0.txt -m $PATH2RES/fslmaths_pve_2.nii.gz\n"
+		fslmeants -i $PATH2RES/rfMRI_REST1_LR_1180.nii.gz -o $PATH2RES/meants_pve_2.txt -m $PATH2RES/fslmaths_pve_2.nii.gz
+	fi
+
+	if [[ "$oneshot" == true ]]; then
+		echo "pileline concluded only for $id, exiting"
+		exit
+	fi
 
 done
